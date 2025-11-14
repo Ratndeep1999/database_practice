6ad5a4648718f498882aa2d69b896e41e11f0e9b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-14 11:09:59 +0530

    appbar add

--- lib/signing_page.dart
+++ lib/signing_page.dart
@@ -11,7 +11,19 @@ class _SigningPageState extends State<SigningPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(
+          "Signing Page",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w600,
+            color: Colors.black87,
+            letterSpacing: 2.0
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange.shade400,
+      ),
       body: SafeArea(child: Column(children: [])),
     );
   }

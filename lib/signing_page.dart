96aecb947e2f95c7e2b9d790eb34a9626f5e8056
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-14 10:57:18 +0530

    Signing page create

---
+++ lib/signing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SigningPage extends StatefulWidget {
+  const SigningPage({super.key});
+
+  @override
+  State<SigningPage> createState() => _SigningPageState();
+}
+
+class _SigningPageState extends State<SigningPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

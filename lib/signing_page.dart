79358d9e1228b72d5c359fc947e9515c765ea7f0
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-14 20:53:55 +0530

    Form widget add with key, Text field add to input email id

--- lib/signing_page.dart
+++ lib/signing_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'CustomWidgets/label_widget.dart';
 
 class SigningPage extends StatefulWidget {
@@ -8,6 +7,9 @@ class SigningPage extends StatefulWidget {
   @override
   State<SigningPage> createState() => _SigningPageState();
 }
+
+final _formKey = GlobalKey<FormState>();
+final _emailController = TextEditingController();
 
 class _SigningPageState extends State<SigningPage> {
   @override
@@ -26,30 +28,84 @@ class _SigningPageState extends State<SigningPage> {
         backgroundColor: Colors.orange.shade400,
       ),
       body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Focus(
+            key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
+              children: [
+                /// Email label
+                LabelWidget(
+                  label: 'Email Address',
+                  fontColor: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w400,
+                ),
+                SizedBox(height: 18.0),
 
-                  /// Email label
-                  LabelWidget(
-                    label: 'Email Address',
-                    fontColor: Colors.black,
-                    fontSize: 20.0,
+                /// Text field for Email
+                TextFormField(
+                  controller: _emailController,
+                  onSaved: (email) {
+                    debugPrint(email);
+                  },
+                  validator: (email) {
+                    return null;
+                  },
+                  // autocorrect: ,
+                  // autofillHints: ,
+                  // onFieldSubmitted: ,
+                  // focusNode: ,
+                  autofocus: false,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black54,
                     fontWeight: FontWeight.w400,
                   ),
-
-                  /// Password label
-                  LabelWidget(
-                    label: 'Password',
-                    fontColor: Colors.black,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w400,
+                  decoration: InputDecoration(
+                    hint: Text('Enter your Email id'),
+                    suffixIcon: Icon(Icons.email, size: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                      borderSide: BorderSide(width: 1.5),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                      borderSide: BorderSide(color: Colors.black26),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                      borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                      borderSide: BorderSide(color: Colors.red, width: 2.0),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                    isDense: true,
                   ),
-                ]
+                ),
+
+                SizedBox(height: 50.0),
+
+                /// Password label
+                LabelWidget(
+                  label: 'Password',
+                  fontColor: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w400,
+                ),
+                SizedBox(height: 18.0),
+              ],
             ),
           ),
+        ),
       ),
     );
   }

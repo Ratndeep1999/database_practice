48bca571af7add4b65787d1f0d11d61769e8778a
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-15 17:10:55 +0530

    signup button add

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:database_practice/CustomWidgets/button_widget.dart';
 import 'package:flutter/material.dart';
 import 'CustomWidgets/label_widget.dart';
 
@@ -23,34 +24,49 @@ class _SignupPageState extends State<SignupPage> {
         centerTitle: true,
         backgroundColor: Colors.orange.shade400,
       ),
-      body: SafeArea(child: Column(children: [
-        /// Email label
-        LabelWidget(
-          label: 'Email Address',
-          fontColor: Colors.black,
-          fontSize: 20.0,
-          fontWeight: FontWeight.w400,
-        ),
-        SizedBox(height: 18.0),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Email label
+              LabelWidget(
+                label: 'Email Address',
+                fontColor: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w400,
+              ),
+              SizedBox(height: 18.0),
 
-        /// Password label
-        LabelWidget(
-          label: 'Password',
-          fontColor: Colors.black,
-          fontSize: 20.0,
-          fontWeight: FontWeight.w400,
-        ),
-        SizedBox(height: 18.0),
+              /// Password label
+              LabelWidget(
+                label: 'Password',
+                fontColor: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w400,
+              ),
+              SizedBox(height: 18.0),
 
-        /// Email label
-        LabelWidget(
-          label: 'Conform password',
-          fontColor: Colors.black,
-          fontSize: 20.0,
-          fontWeight: FontWeight.w400,
+              /// Email label
+              LabelWidget(
+                label: 'Conform password',
+                fontColor: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w400,
+              ),
+              SizedBox(height: 18.0),
+
+              /// Signup Button
+              ButtonWidget(
+                label: 'Create Account',
+                buttonPress: () {},
+                icon: Icons.account_circle,
+              ),
+            ],
+          ),
         ),
-        SizedBox(height: 18.0),
-      ],)),
+      ),
     );
   }
 }

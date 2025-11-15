da21fd1f7b27852a678d9681af61d18ea763ee86
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-15 17:02:50 +0530

    email, password and conform password label add using custom label widget

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'CustomWidgets/label_widget.dart';
 
 class SignupPage extends StatefulWidget {
@@ -24,7 +23,34 @@ class _SignupPageState extends State<SignupPage> {
         centerTitle: true,
         backgroundColor: Colors.orange.shade400,
       ),
-      body: SafeArea(child: Column(children: [],)),
+      body: SafeArea(child: Column(children: [
+        /// Email label
+        LabelWidget(
+          label: 'Email Address',
+          fontColor: Colors.black,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w400,
+        ),
+        SizedBox(height: 18.0),
+
+        /// Password label
+        LabelWidget(
+          label: 'Password',
+          fontColor: Colors.black,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w400,
+        ),
+        SizedBox(height: 18.0),
+
+        /// Email label
+        LabelWidget(
+          label: 'Conform password',
+          fontColor: Colors.black,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w400,
+        ),
+        SizedBox(height: 18.0),
+      ],)),
     );
   }
 }

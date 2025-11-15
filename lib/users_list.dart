a60f836622e73b86d47c381ec353aac321d8e908
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-15 16:05:02 +0530

    layout and appbar add

--- lib/users_list.dart
+++ lib/users_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'CustomWidgets/label_widget.dart';
+
 class UsersList extends StatefulWidget {
   const UsersList({super.key});
 
@@ -10,6 +12,19 @@ class UsersList extends StatefulWidget {
 class _UsersListState extends State<UsersList> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: LabelWidget(
+          label: "Users List",
+          fontSize: 30,
+          fontWeight: FontWeight.w600,
+          fontColor: Colors.black87,
+          letterSpacing: 2.0,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange.shade400,
+      ),
+      body: SafeArea(child: Column(children: [],)),
+    );
   }
 }

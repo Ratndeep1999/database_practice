e33b5ec9c02c3887d44b6666cf5302ee1011812b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-15 15:53:35 +0530

    clickable text widget refactor and function and label parameter pass

---
+++ lib/CustomWidgets/clickable_text_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ClickableTextWidget extends StatelessWidget {
+  const ClickableTextWidget({
+    super.key,
+    required this.click,
+    required this.label,
+  });
+
+  final VoidCallback click;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: click,
+      child: Text(
+        label,
+        style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.w500,
+          color: Colors.black45,
+        ),
+      ),
+    );
+  }
+}

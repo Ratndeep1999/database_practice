d091e7dec05dd954ca930efb89b1e295de5dfbbd
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-15 12:26:12 +0530

    buttom refactored anf parameter passed

---
+++ lib/CustomWidgets/button_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({
+    super.key,
+    required this.label,
+    required this.buttonPress,
+  });
+
+  final String label;
+  final VoidCallback buttonPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton.icon(
+        onPressed: buttonPress,
+        label: Text(label),
+        icon: Icon(Icons.login),
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty('buttonPress', buttonPress));
+  }
+}

f7651c43d9a92505e94366b468b14985593e5764
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-15 17:19:12 +0530

    app abr add

--- lib/forget_password.dart
+++ lib/forget_password.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'CustomWidgets/label_widget.dart';
+
 class ForgetPassword extends StatelessWidget {
   const ForgetPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: LabelWidget(
+          label: "Forget Password",
+          fontSize: 30,
+          fontWeight: FontWeight.w600,
+          fontColor: Colors.black87,
+          letterSpacing: 2.0,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange.shade400,
+      ),
       body: Column(),
     );
   }

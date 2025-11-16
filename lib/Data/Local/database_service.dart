3b0d1bd41facc3ce35007bc68c8290c363762f74
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-16 19:01:25 +0530

    Singleton pattern add

--- lib/Data/Local/database_service.dart
+++ lib/Data/Local/database_service.dart
@@ -1,6 +1,14 @@
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseService {
+
+  // Singleton pattern
+  static final DatabaseService _instance = DatabaseService.internal();
+  DatabaseService.internal();
+  factory DatabaseService (){
+    return _instance;
+  }
+
   /// Database Object
   static Database? _database;
 }

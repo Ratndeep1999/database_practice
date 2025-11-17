273662a2b5b3fd436cd522ef91c888da629e1298
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-17 17:52:28 +0530

    custom text fields add for both user name and email address

--- lib/CustomWidgets/user_edit_bottom_sheet.dart
+++ lib/CustomWidgets/user_edit_bottom_sheet.dart
@@ -1,5 +1,5 @@
+import 'package:database_practice/CustomWidgets/input_text_field_widget.dart';
 import 'package:flutter/material.dart';
-
 import 'label_widget.dart';
 
 class UserEditBottomSheet extends StatefulWidget {
@@ -10,6 +10,16 @@ class UserEditBottomSheet extends StatefulWidget {
 }
 
 class _UserEditBottomSheetState extends State<UserEditBottomSheet> {
+  late final TextEditingController userNameController;
+  late final TextEditingController emailController;
+
+  @override
+  void initState() {
+    super.initState();
+    userNameController = TextEditingController();
+    emailController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -37,7 +47,15 @@ class _UserEditBottomSheetState extends State<UserEditBottomSheet> {
             fontWeight: FontWeight.w400,
           ),
           SizedBox(height: 8.0),
-
+          InputTextFieldWidget(
+            controller: userNameController,
+            keyboardType: TextInputType.name,
+            isSuffixIcon: false,
+            onSaved: (value) {},
+            validation: (value) {
+              return null;
+            },
+          ),
           SizedBox(height: 18.0),
 
           /// Email Address Label
@@ -48,22 +66,27 @@ class _UserEditBottomSheetState extends State<UserEditBottomSheet> {
             fontWeight: FontWeight.w400,
           ),
           SizedBox(height: 8.0),
-
+          InputTextFieldWidget(
+            controller: emailController,
+            keyboardType: TextInputType.name,
+            isSuffixIcon: false,
+            onSaved: (value) {},
+            validation: (value) {
+              return null;
+            },
+          ),
           SizedBox(height: 50.0),
-
 
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
+
             /// Button Section
             child: Row(
               children: [
                 /// Save Button
                 SizedBox(
                   width: 150,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Save"),
-                  ),
+                  child: ElevatedButton(onPressed: () {}, child: Text("Save")),
                 ),
                 Spacer(),
 

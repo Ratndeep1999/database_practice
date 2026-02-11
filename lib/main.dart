aabc5d5ade4fe7a8d8f5c8ef186f72a0f10e2627
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-11 09:47:21 +0530

    main.dart: WidgetsFlutterBinding.ensureInitialized(); line add.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:database_practice/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 

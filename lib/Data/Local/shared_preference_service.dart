999f0ade94125c44e32a68678b8fb159a2fcc060
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-11 08:59:12 +0530

    shared_preference_service.dart: SharedPreferenceService class add, and singleton pattern add.

---
+++ lib/Data/Local/shared_preference_service.dart
@@ -0,0 +1,11 @@
+class SharedPreferenceService {
+  /// Singleton pattern
+  static final SharedPreferenceService _instance =
+      SharedPreferenceService.internal();
+
+  SharedPrefService.internal();
+
+  factory SharedPrefService() => _instance;
+
+  static
+}

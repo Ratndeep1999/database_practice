6701541b5391b782d0a86c32a00499c91b2fde32
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-11 09:55:38 +0530

    splash_page.dart: splash page with functionality add.

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,3 +1,5 @@
+import 'package:database_practice/Data/Local/shared_preference_service.dart';
+import 'package:database_practice/signing_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -8,10 +10,39 @@ class SplashPage extends StatefulWidget {
 }
 
 class SplashPageState extends State<SplashPage> {
+  SharedPreferenceService prefs = SharedPreferenceService();
+
+  @override
+  void initState() {
+    super.initState();
+    whereToGo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Center(child: Text("My Data"))),
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            "This is Splash Page..",
+            style: TextStyle(fontSize: 30.0),
+          ),
+        ),
+      ),
     );
+  }
+
+  /// Navigate base on value
+  void whereToGo() async {
+    final loginStatus = await prefs.getLoginStatus;
+    Future.delayed(const Duration(seconds: 3), () {
+      if (loginStatus) {
+        /// ToDo: Navigate to HomePage
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => SigningPage()),
+        );
+      }
+    });
   }
 }

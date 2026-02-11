7074d539af94de13215c1e4c18992e34165bd73f
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-11 09:18:47 +0530

    shared_preference_service.dart: shared preference object get, methods add to read and write login status, and method add to clear all stored values.

--- lib/Data/Local/shared_preference_service.dart
+++ lib/Data/Local/shared_preference_service.dart
@@ -1,11 +1,38 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class SharedPreferenceService {
   /// Singleton pattern
   static final SharedPreferenceService _instance =
       SharedPreferenceService.internal();
 
-  SharedPrefService.internal();
+  SharedPreferenceService.internal();
 
-  factory SharedPrefService() => _instance;
+  factory SharedPreferenceService() => _instance;
 
-  static
+  /// SharedPreference object get
+  static SharedPreferences? _prefs;
+
+  Future<SharedPreferences> get _sharedPrefs async {
+    return _prefs ??= await SharedPreferences.getInstance();
+  }
+
+  static const String _kIsLoggedIn = "isLoggedIn";
+
+  /// Check login status
+  Future<bool> get getLoginStatus async {
+    final prefs = await _sharedPrefs;
+    return prefs.getBool(_kIsLoggedIn) ?? false;
+  }
+
+  /// Set login value
+  Future<void> setLoginValue() async {
+    final prefs = await _sharedPrefs;
+    prefs.setBool(_kIsLoggedIn, true);
+  }
+
+  /// Clear all stored values
+  Future<void> clearAll() async {
+    final prefs = await _sharedPrefs;
+    await prefs.clear();
+  }
 }
